0f633c71b006ba7c43671feef1c66c83708857a1
Ingris Andrade <iac22@outlook.com>
2022-08-08 20:54:53 -0300

    refatora strictmode

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-BrowserRouter
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root'),
 );
