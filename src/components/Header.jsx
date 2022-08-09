205abcc8bcf8626bad5dc28682ac53ef0f4fe5d4
Ingris Andrade <iac22@outlook.com>
2022-08-08 23:29:36 -0300

    Recupera nome da pessoa logada e finaliza req

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  state = {
+    inputUser: '',
+    loading: true,
+  }
+
+  async componentDidMount() {
+    this.logName();
+  }
+
+  logName = () => {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      this.setState({ loading: false, inputUser: user.name });
+    });
+  }
+
   render() {
+    const {
+      inputUser,
+      loading,
+    } = this.state;
+
+    if (loading) return <Loading />;
+
     return (
       <div>
         <header data-testid="header-component">
-          Header
+          <p data-testid="header-user-name">{ inputUser }</p>
         </header>
       </div>
     );
