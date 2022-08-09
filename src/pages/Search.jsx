edbc9dc3c702ea718daf4a3be3cdeaf3a4dfab99
Ingris Andrade <iac22@outlook.com>
2022-08-09 00:41:50 -0300

    Habilita btn com 2 ou + caract e finaliza req 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state ={
+    btnDisabled: true,
+    searchArtist: '',
+  }
+
+  inputChange = ({ name, value }) => {
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.habilitateBtn();
+    });
+  }
+
+  habilitateBtn = () => {
+    const { searchArtist } = this.state;
+    const caracterMin = 2;
+    const validateCaracter = searchArtist.length >= caracterMin;
+    this.setState({
+      btnDisabled: !validateCaracter,
+    });
+  }
+
   render() {
+    const {
+      searchArtist,
+      btnDisabled,
+    } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
@@ -10,13 +37,16 @@ class Search extends Component {
         <form>
           <input
             type="text"
-            name=""
-            id=""
+            name="searchArtist"
+            value={ searchArtist }
+            placeholder="Digite nome do artista"
             data-testid="search-artist-input"
+            onChange={ ({ target }) => this.inputChange(target) }
           />
           <button
             type="button"
             data-testid="search-artist-button"
+            disabled={ btnDisabled }
           >
             Pesquisar
           </button>
