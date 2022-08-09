1b7fb38511f026dbbed4729685d91470fb5aa979
Ingris Andrade <iac22@outlook.com>
2022-08-08 21:13:33 -0300

    Cria campo de input para nome

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <label htmlFor="input-name-login">
+          <input
+            type="text"
+            name="name"
+            id="input-name-login"
+            data-testid="login-name-input"
+          />
+        </label>
+      </div>
     );
   }
 }
