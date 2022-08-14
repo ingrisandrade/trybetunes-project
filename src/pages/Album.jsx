7cc76cac44b40a8b61150c656211e40e16830baf
Ingris Andrade <iac22@outlook.com>
2022-08-14 14:53:44 -0300

    Cria requisição cpm função getMusics

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Album extends Component {
+  state = {
+    albumArtist: [],
+  }
+
+  componentDidMount () {
+    this.findSongs();
+  }
+
+  findSongs() {
+    const { match: { params: { id }}
+    } = this.props;
+    const displayMusic = async () => {
+      const songs = await getMusics(id);
+      this.setState({
+        albumArtist: songs
+      });
+    };
+    getMusics
+  }
+
   render() {
     return (
       <div data-testid="page-album">
