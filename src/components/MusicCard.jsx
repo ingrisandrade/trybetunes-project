f1b6b19f01f8e69f3e2acd52b566dd5aa7e70569
Ingris Andrade <iac22@outlook.com>
2022-08-14 22:56:38 -0300

    Lista todas as músicas retornadas pela API

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MusicCard extends Component {
   render() {
+    const {
+      albumArtist,
+    } = this.props;
     return (
-      <div>MusicCard</div>
+      <div>
+        <p>MusicCard</p>
+        {
+          albumArtist.map((songs) => (
+            <div key={ songs.trackName }>
+              <p>
+                { songs.trackName }
+              </p>
+              <audio
+                data-testid="audio-component"
+                src="{previewUrl}"
+                controls
+              >
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                {' '}
+                <code>audio</code>
+                .
+              </audio>
+            </div>
+          ))
+        }
+      </div>
     );
   }
 }
+
+MusicCard.propTypes = {
+  albumArtist: PropTypes.string,
+}.isRequired;
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,36 +1,68 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
   state = {
     albumArtist: [],
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.findSongs();
   }
 
   findSongs() {
-    const { match: { params: { id }}
+    const { match: { params: { id } },
     } = this.props;
     const displayMusic = async () => {
       const songs = await getMusics(id);
       this.setState({
-        albumArtist: songs
+        albumArtist: songs,
       });
     };
-    getMusics
+    displayMusic();
   }
 
   render() {
+    const {
+      albumArtist,
+    } = this.state;
+    let collections = [];
+    if (albumArtist.length > 0) {
+      collections = [albumArtist[0]];
+    }
     return (
       <div data-testid="page-album">
-        <p data-testid="artist-name">Banda/Artista</p>
-        <p data-testid="album-name">Álbum/Banda/Artista</p>
         <Header />
+        {
+          collections.map((collection) => (
+            <div key={ collection.collectionName }>
+              <div>
+                <img
+                  src={ collection.artworkUrl100 }
+                  alt={ `imageMusic: ${collection.trackName}` }
+                />
+              </div>
+              <p data-testid="album-name">
+                {collection.collectionName}
+                {collection.artistName}
+              </p>
+              <p data-testid="artist-name">
+                { collection.artistName }
+              </p>
+            </div>
+          ))
+        }
+        <MusicCard albumArtist={ albumArtist.slice(1) } />
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.string,
+}.isRequired;
 
 export default Album;
