aadee8eb596449102f8565ccb6bc062545bb505c
Ingris Andrade <iac22@outlook.com>
2022-08-24 22:13:40 -0300

    Inicia requisito 11

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
