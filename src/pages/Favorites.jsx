ee3d48b24db0fb22be34045c068297853e18863d
Ingris Andrade <iac22@outlook.com>
2022-08-24 23:05:01 -0300

    Add função de favoritar

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -9,6 +9,30 @@ class Favorites extends Component {
     loading: false,
     listFavSongs: [],
   }
+
+  async componentDidMount() {
+    const favLocalStorage = await getFavoriteSongs();
+
+    this.setState({
+      loading: true,
+      listFavSongs: favLocalStorage,
+    });
+  }
+
+  handleFav = async (id) => {
+    const {
+      listFavSongs
+    } = this.state;
+    this.setState({ loading: false });
+
+    const favSong = listFavSongs.find((song) => song.trackId === trackId);
+    await removeSong(favSong);
+    const favLocalStorage = await getFavoriteSongs();
+    this.setState({
+      loading: true,
+      listFavSongs: favLocalStorage,
+    });
+  }
   render() {
     return (
       <div data-testid="page-favorites">
